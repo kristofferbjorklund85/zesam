21e9c4e6afac5ff0f261206a435063f82d8713d1
Pregmark <sam.pregmark@gmail.com>
2018-04-18 18:15:05 +0200

    Working on Toolbar design

--- app/src/main/java/zesam/src/Login.java
+++ app/src/main/java/zesam/src/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.support.v7.widget.Toolbar;
 
 public class Login extends AppCompatActivity{
 
@@ -11,6 +12,9 @@ public class Login extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar t = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(t);
     }
 
     public void signIn(View v) {
