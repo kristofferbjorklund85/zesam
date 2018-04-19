01be77326df5e0a60ba62502ad2cb5d97cd3f6e2
kristofferbjorklund85 <kristofferbjorklund85@gmail.com>
2018-04-19 11:55:19 +0200

    Added a file to cache username and password

--- app/src/main/java/zesam/src/Login.java
+++ app/src/main/java/zesam/src/Login.java
@@ -5,20 +5,71 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Login extends AppCompatActivity{
+
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        checkCache();
+
         Toolbar t = (Toolbar) findViewById(R.id.toolbar_logged_out);
         setSupportActionBar(t);
     }
 
     public void signIn(View v) {
+        saveCred(username.getText().toString(), password.getText().toString());
         Intent intent = new Intent(this, PickAccountSpinner.class);
         startActivity(intent);
     }
+
+    private void saveCred(String user, String pass) {
+        String filename = "cache.txt";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, this.MODE_PRIVATE);
+            outputStream.flush();
+            outputStream.write(user.getBytes());
+            outputStream.write("\n".getBytes());
+            outputStream.write(pass.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void checkCache() {
+        username = (EditText) findViewById(R.id.username_text);
+        password = (EditText) findViewById(R.id.password_text);
+
+        username.setText(getIntent().getStringExtra("User"));
+        password.setText(getIntent().getStringExtra("Pass"));
+
+    }
+
+    private void clearCache() {
+        String filename = "cache.txt";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, this.MODE_PRIVATE);
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- app/src/main/java/zesam/src/MainActivity.java
+++ app/src/main/java/zesam/src/MainActivity.java
@@ -3,15 +3,63 @@ package zesam.src;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, Login.class);
+        intent = new Intent(this, Login.class);
+        checkCache();
+
         startActivity(intent);
     }
+
+    private void checkCache() {
+
+        try {
+            InputStream inputStream = this.openFileInput("cache.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                ArrayList<String> list = new ArrayList<>();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    list.add(receiveString);
+                }
+
+                inputStream.close();
+                Log.d("Output", list.get(0));
+                Log.d("Output", list.get(1));
+                intent.putExtra("User", list.get(0));
+                intent.putExtra("Pass", list.get(1));
+
+            }
+
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        } catch (IndexOutOfBoundsException e) {
+            Log.e("login activity", "Index Out of Bounds: " + e.toString());
+        }
+
+    }
+
+
 }
