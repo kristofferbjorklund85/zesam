0c3774ddc746c9973291fac63e3264af5d804cc5
kristofferbjorklund85 <kristofferbjorklund85@gmail.com>
2018-04-27 10:17:22 +0200

    Added reminder to Review, finished previous activitys

--- app/src/main/java/zesam/src/Review.java
+++ app/src/main/java/zesam/src/Review.java
@@ -64,9 +64,6 @@ public class Review extends AppCompatActivity {
             createReminder();
         }
 
-        /*Intent intent = new Intent(this, PickAccountSpinner.class);
-        startActivity(intent);*/
-
         finish();
     }
 
@@ -105,107 +102,6 @@ public class Review extends AppCompatActivity {
         startActivity(intent);
 
     }
-
-
-    /*
-    public void createReminder() {
-        while(!checkPermissions()) {
-
-        }
-
-        Date currentTime = Calendar.getInstance().getTime();
-
-        String title = "Test";
-        String desc = "Testar testar testar";
-        String place = "Diadrom";
-        long startDate = Calendar.getInstance().getTimeInMillis() + 3600000;
-        long eventID;
-
-
-        try {
-            String eventUriString = "content://com.android.calendar/events";
-            ContentValues eventValues = new ContentValues();
-            eventValues.put("calendar_id", 3); // id, We need to choose from
-            // our mobile for primary its 1
-            eventValues.put("title", title);
-            eventValues.put("description", desc);
-            eventValues.put("eventLocation", place);
-
-            long endDate = startDate + 1000 * 10 * 10; // For next 10min
-            eventValues.put("dtstart", startDate);
-            eventValues.put("dtend", endDate);
-            //eventValues.put("duration", "PT1H");
-
-            eventValues.put("allDay", 0); //If it is bithday alarm or such
-            // kind (which should remind me for whole day) 0 for false, 1
-            // for true
-            eventValues.put("eventStatus", 1); // This information is
-            // sufficient for most
-            // entries tentative (0),
-            // confirmed (1) or canceled
-            // (2):
-            eventValues.put("eventTimezone", TimeZone.getDefault().getID());
-            *//*
-             * Comment below visibility and transparency column to avoid
-             * java.lang.IllegalArgumentException column visibility is invalid
-             * error
-             *//*
-
-            // eventValues.put("visible", 1); // visibility to default (0),
-            // confidential (1), private
-            // (2), or public (3):
-            // eventValues.put("transparency", 0); // You can control whether
-            // an event consumes time
-            // opaque (0) or transparent
-            // (1).
-            eventValues.put("hasAlarm", 1); // 0 for false, 1 for true
-
-            Uri eventUri = this.getApplicationContext()
-                    .getContentResolver()
-                    .insert(Uri.parse(eventUriString), eventValues);
-            eventID = Long.parseLong(eventUri.getLastPathSegment());
-
-            String reminderUriString = "content://com.android.calendar/reminders";
-            ContentValues reminderValues = new ContentValues();
-            reminderValues.put("event_id", eventID);
-            reminderValues.put("minutes", 5); // Default value
-            //set time in min which occur before event start
-            reminderValues.put("method", 1); // Alert Methods: Default(0),
-            // Alert(1), Email(2),SMS(3)
-            Uri reminderUri = this.getApplicationContext()
-                    .getContentResolver()
-                    .insert(Uri.parse(reminderUriString), reminderValues);
-
-
-        } catch (Exception ex) {
-            Log.d("Reminder: ","Error in adding event on calendar" + ex.getMessage());
-        }
-
-
-
-    }
-
-    */
-
-    private boolean checkPermissions() {
-        int MY_PERMISSIONS_REQUEST_CALENDAR = 1;
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_CALENDAR)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR},
-                    MY_PERMISSIONS_REQUEST_CALENDAR);
-
-            return true;
-        } else {
-            // Permission has already been granted
-            return true;
-        }
-    }
-
-
 
 }
 
