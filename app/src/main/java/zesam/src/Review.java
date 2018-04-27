307bf92a8a357cd5349dd0664f84707e93e5f674
Pregmark <sam.pregmark@gmail.com>
2018-04-27 12:17:12 +0200

    Made maps link clickable

--- app/src/main/java/zesam/src/Review.java
+++ app/src/main/java/zesam/src/Review.java
@@ -14,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -63,10 +64,9 @@ public class Review extends AppCompatActivity {
 
         getUserLocation();
 
-        text = text + mapsURL + userLocation.getLatitude() + "," + userLocation.getLongitude();
-
         resultText = (TextView) findViewById(R.id.resultText);
-        resultText.setText(text);
+        resultText.setClickable(true);
+        resultText.setText(text + mapsURL + userLocation.getLatitude() + "," + userLocation.getLongitude());
 
     }
 
