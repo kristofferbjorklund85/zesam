e520d749f2949aa72f8cda41227a4c42ba3fa11d
kristofferbjorklund85 <kristofferbjorklund85@gmail.com>
2018-05-03 14:23:41 +0200

    started on PlanMeeting

--- app/src/main/java/zesam/src/PlanMeeting.java
+++ app/src/main/java/zesam/src/PlanMeeting.java
@@ -1,8 +1,15 @@
 package zesam.src;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.GeoDataClient;
@@ -12,35 +19,91 @@ import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlanMeeting extends AppCompatActivity {
 
-    private GeoDataClient mGeoDataClient;
-    private PlaceDetectionClient mPlaceDetectionClient;
+    FakeData fd;
+    ArrayList<FakeData.Meeting> list;
 
-    private String location;
+    private RecyclerView recyclerView;
+    private MeetingAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_meeting);
 
-        mGeoDataClient = Places.getGeoDataClient(this, null);
+        Toolbar t = findViewById(R.id.toolbar_logged_in);
+        setSupportActionBar(t);
 
-        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+        recyclerView = (RecyclerView) findViewById(R.id.plan_recycler);
 
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        list = new ArrayList<>();
+        adapter = new MeetingAdapter(this, list);
 
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-                location = place.getName().toString();
-            }
+        recyclerView.setAdapter(adapter);
 
-            @Override
-            public void onError(Status status) {
-                Log.d("PlaceSelected", "An error occurred: " + status);
-            }
-        });
+        fd = new FakeData();
+        list = fd.getFutureMeetings();
+
+
     }
+
+
+
+
+
+
+    public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.MyViewHolder> {
+        private Context mContext;
+        private List<FakeData.Meeting> meetList;
+
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            public TextView listmeetingdate, listmeetingorganizer, listmeetingcompany, listmeetingcontact, listmeetingdesc;
+
+            public MyViewHolder(View view) {
+                super(view);
+
+                listmeetingdate = (TextView) view.findViewById(R.id.listmeetingdate);
+                listmeetingorganizer = (TextView) view.findViewById(R.id.listmeetingorganizer);
+                listmeetingcompany = (TextView) view.findViewById(R.id.listmeetingcompany);
+                listmeetingcontact = (TextView) view.findViewById(R.id.listmeetingcontact);
+                listmeetingdesc = (TextView) view.findViewById(R.id.listmeetingdesc);
+            }
+        }
+
+        public MeetingAdapter(Context mContext, List<FakeData.Meeting> meetList) {
+            this.mContext = mContext;
+            this.meetList = meetList;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.meetingcard, parent, false);
+
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(final MyViewHolder holder, int position) {
+            FakeData.Meeting meet = meetList.get(position);
+            holder.listmeetingdate.setText(meet.date);
+            holder.listmeetingorganizer.setText(meet.organizer);
+            holder.listmeetingcompany.setText(meet.comapnyName);
+            holder.listmeetingcontact.setText(meet.contact);
+            holder.listmeetingdesc.setText(meet.description);
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return meetList.size();
+        }
+    }
+
 }
